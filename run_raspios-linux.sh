c8753e28ad7b7759a1b1d8347a0b5adc111443a6
Petr Ruzicka <petr.ruzicka@gmail.com>
2021-12-17 06:07:24 +0100

    Script update

--- run_raspios-linux.sh
+++ run_raspios-linux.sh
@@ -1,6 +1,16 @@
 #!/bin/bash -eux
 
-DEVICE="sda"
+DEVICE="mmcblk0"
+
+wget -C https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2021-11-08/2021-10-30-raspios-bullseye-armhf-lite.zip
+
+cat > owner.txt << EOF
+Petr Ruzicka
+
+petr.ruzicka@gmail.com
+
+Czech Republic
+EOF
 
 lsblk --output NAME,MODEL,MODEL | grep ${DEVICE}
 
@@ -10,17 +20,17 @@ zcat ./*raspios*.zip | sudo dd of=/dev/${DEVICE} bs=4M
 sudo partprobe /dev/${DEVICE}
 
 MYTMP=$(mktemp --directory)
-sudo mount /dev/${DEVICE}1 "${MYTMP}"
+sudo mount /dev/${DEVICE}p1 "${MYTMP}"
 sudo touch "${MYTMP}/ssh"
-sudo cp ~/Documents/owner.txt "${MYTMP}/"
+sudo cp owner.txt "${MYTMP}/"
 sudo umount "${MYTMP}"
 
-sudo mount "/dev/${DEVICE}2" "${MYTMP}"
+sudo mount "/dev/${DEVICE}p2" "${MYTMP}"
 sudo bash -c "cat >> ${MYTMP}/etc/dhcpcd.conf" << EOF
 interface eth0
 static ip_address=192.168.1.2/24
 static routers=192.168.1.1
 static domain_name_servers=1.1.1.1
 EOF
-sudo cp ~/Documents/owner.txt "${MYTMP}/"
+sudo cp owner.txt "${MYTMP}/"
 sudo umount "${MYTMP}"
